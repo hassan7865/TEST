f988f12d5725a42b27c269f4e42499b81d6c0ce5
Hassan Siddiqui <hassansiddiqui7865@gmail.com>
2023-02-22 20:38:59 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import axios from 'axios'
 function App() {
   const handleClick = async()=>{
-    const res = await axios.get("http://localhost:3000")
+    const res = await axios.get("http://localhost:8888/data")
   }
   return (
     <div className="App">
